9af1c68e2510495c62f25a47ffedb09938ecc36a
SensorMart <sensormart.mkt@gmail.com>
2024-07-30 15:41:37 +0545

    here data and its route of /data is working but there are variations in data

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, WebSocket, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, WebSocket, Request ,HTTPException
+from fastapi.responses import HTMLResponse ,JSONResponse
 from fastapi.templating import Jinja2Templates
 from sensor_data.data_reader import SensorDataReader
 import asyncio
+import json
 
 app = FastAPI()
 templates =  Jinja2Templates(directory= "templates")
 
 #initialize the sensor data reader
-sensor_data_reader = SensorDataReader(port='COM7', baud_rate='115200')
+sensor_data_reader = SensorDataReader(port='COM6', baud_rate='115200')
 
 @app.get("/",response_class=HTMLResponse)
 async def get_webpage(request: Request):
@@ -20,9 +21,26 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         data = sensor_data_reader.get_data()
         if data:
-            await websocket.send_text(data)
+            await websocket.send_text(json.dumps(data[-1])) #sends the latest data points
         await asyncio.sleep(0.005)
 
+@app.get("/data/")
+async def get_data():
+    data = sensor_data_reader.get_data()
+    if data:
+        return JSONResponse(content={"data":data})
+    else:
+        raise HTTPException(status_code=404, detail="no data")
+    
+@app.post("/data/")
+async def update_sensor_data(request: Request):
+    # This endpoint can be used to simulate updates or interact with data in other ways
+    data = sensor_data_reader.get_data()
+    if data:
+        return JSONResponse(content={"data": data})
+    else:
+        raise HTTPException(status_code=404, detail="No data available")
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host ="localhost",port = 8000)
--- sensor_data/data_reader.py
+++ sensor_data/data_reader.py
@@ -1,12 +1,13 @@
 import serial
 import threading
+import json
 
 class SensorDataReader:
     def __init__(self, port, baud_rate):
         self.port = port
         self.baud_rate = baud_rate
         self.serial_connection = serial.Serial(self.port, self.baud_rate)
-        self.data = None
+        self.data = []
         self.lock = threading.Lock()
         self.read_thread = threading.Thread(target=self.read_data)
         self.read_thread.start()
@@ -14,9 +15,23 @@ class SensorDataReader:
     def read_data(self):
         while True:
             if self.serial_connection.in_waiting > 0:
+                
                 data_line = self.serial_connection.readline().decode('utf-8')
+                #spliting data in srno,x,y,z
+                part = data_line.split(',')
+                if len(part) == 4:
+                    data_point = {
+                        "sr_no": part[0],
+                        "X":part[1],
+                        "Y":part[2],
+                        "Z":part[3]
+                    }
+                    #to store the data in list
                 with self.lock:
-                    self.data = data_line
+                    self.data.append(data_point)
+                    #set the limit of ther list
+                    if len(self.data)>1000:
+                        self.data.pop(0)
 
     def get_data(self):
         with self.lock:
